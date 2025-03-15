58db7a6ce47766f8e75420bf49b2eabc9738bb1b
João P. V. Silveira <joaopedrovieirasilveira826@gmail.com>
2025-03-15 09:28:11 -0300

    Create PIDEasy.h

---
+++ PIDEasy.h
@@ -0,0 +1,24 @@
+#ifndef PIDEASY_H
+#define PIDEASY_H
+
+class PID {
+  public:
+    PID(float kp, float ki, float kd);
+
+    float compute(float error, unsigned long dt);
+    void setWindUP(float min, float max);
+    void setConstrain(int min, int max);
+    void setSmoothingDerivate(float sD);
+    void setDampingFactor(float dF);
+
+  private:
+    float kp, ki, kd;
+    float previous_error;
+    float previous_derivative;
+    float integral;
+    float min_windup, max_windup;
+    float derivative_smoothing, dampingFactor;
+    int min_constrain, max_constrain;
+};
+
+#endif
